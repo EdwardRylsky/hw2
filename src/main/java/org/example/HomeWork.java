8cc8a6537090224df00c1056165457ef91dffb3f
Edward_Rylsky <edwardrylsky@gmail.com>
2024-09-21 23:20:55 +0500

    hw2 feat findMaxDigits

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,14 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
 
 
 public class HomeWork extends BinaryIntSearchTree {
 
     /**
      * <h1>Задание 1.</h1>
-     * Дан класс BinaryTree, который реализует бинарное дерево поиска.
-     * Реализовать метод findMaxDigits, который возвращает массив
+     * Дан класс BinaryIntSearchTree, который реализует бинарное дерево поиска.
+     * Реализовать метод findMaxDigits, который возвращает список
      * наибольших элементов в дереве, не превосходящих upperBound.
      * <br/>
      * Пример :
@@ -23,9 +26,57 @@ public class HomeWork extends BinaryIntSearchTree {
      * Сигнатуру метода не меняем
      */
     public List<Integer> findMaxDigits(int count, int upperBound) {
-        //TODO реализовать метод
-        return null;
+        if (count < 1) {
+            throw new IllegalArgumentException("Count have to be greater then 0");
+        }
+
+        List<Integer> entrySet = entrySet();
+        entrySet.sort(Comparator.comparingInt(Integer::intValue).reversed());
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < entrySet.size() && result.size() < count; i++) {
+            if (entrySet.get(i) <= upperBound) {
+                result.add(entrySet.get(i));
+            }
+        }
+
+        return result;
     }
 
+    public List<Integer> entrySet() {
+        List<Integer> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node current = stack.pop();
 
+            if (list.contains(current.value)) {
+                continue;
+            }
+            if (current.right != null) {
+                if (!list.contains(current.right.value)) {
+                    stack.push(current);
+                    stack.push(current.right);
+                    continue;
+                }
+                list.add(current.value);
+            }
+
+            if (current.left != null) {
+                if (!list.contains(current.left.value)) {
+                    stack.push(current);
+                    stack.push(current.left);
+                    continue;
+                }
+                list.add(current.value);
+            }
+
+            if (current.left == null && current.right == null) {
+                list.add(current.value);
+            }
+        }
+
+        return list;
+    }
 }
--- src/test/java/org/example/BinaryIntSearchTreeTest.java
+++ src/test/java/org/example/BinaryIntSearchTreeTest.java
@@ -2,19 +2,33 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryIntSearchTreeTest {
-
     @Test
     void add() {
-        BinaryIntSearchTree tree = new BinaryIntSearchTree();
+        HomeWork tree = new HomeWork();
 
         tree.add(5);
-
-        for (int i = 0; i < 10; i++) {
-            tree.add(i);
-        }
+        tree.add(3);
+        tree.add(1);
+        tree.add(2);
+        tree.add(4);
+        tree.add(8);
+        tree.add(7);
+        tree.add(6);
         tree.add(10);
+        tree.add(9);
+
+        assertEquals(asList(1), tree.findMaxDigits(1, 1));
+        assertEquals(asList(2, 1), tree.findMaxDigits(2, 2));
+        assertEquals(asList(3, 2, 1), tree.findMaxDigits(3, 3));
+        assertEquals(asList(4, 3, 2, 1), tree.findMaxDigits(4, 4));
+        assertEquals(asList(5, 4, 3, 2, 1), tree.findMaxDigits(5, 5));
+        assertEquals(asList(9, 8, 7, 6, 5, 4), tree.findMaxDigits(6, 9));
+        assertEquals(asList(9, 8, 7, 6, 5, 4, 3), tree.findMaxDigits(7, 9));
+        assertEquals(asList(9, 8, 7, 6, 5, 4, 3, 2), tree.findMaxDigits(8, 9));
+        assertEquals(asList(9, 8, 7, 6, 5, 4, 3, 2, 1), tree.findMaxDigits(9, 9));
     }
 }
